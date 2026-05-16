742b8b6c25bfa15e50184e2ee0394245a74af00d
Eduardo Feliciano <eduardofeliciano74@gmail.com>
2026-05-15 22:18:33 -0300

    feat: Adicionando arquivo que vai ter todos os includes do jogo, para melhor organização

---
+++ Pokecin/jogo.hpp
@@ -0,0 +1,15 @@
+// jogo.hpp
+#ifndef JOGO_HPP
+#define JOGO_HPP
+
+// Principais bibliotecas que vamos utilizar
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+
+// Todos os arquivos .hpp para organização
+#include "intro.hpp"
+#include "ataques.hpp"
+
+#endif
